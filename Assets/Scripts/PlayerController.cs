4e7206d47b76c86256dcf10d50ea21b98fe36a3e
Gerchis <56065317+Gerchis@users.noreply.github.com>
2020-02-01 12:21:55 +0100

    Gravity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity;
+    public Transform worldCenter;
+    public Rigidbody2D rb;
+
+    private Vector2 gravityDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        gravityDirection.Set(transform.position.x - worldCenter.position.x, transform.position.y - worldCenter.position.y);
+    }
+
+    private void FixedUpdate()
+    {
+        float delta = Time.fixedDeltaTime * 1000;
+
+        rb.velocity = gravityDirection.normalized * -gravity * delta;
+    }
+}

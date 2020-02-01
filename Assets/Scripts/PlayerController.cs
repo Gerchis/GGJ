b84588c0f2c1b8fa345953ae18a9c631e51c56b8
Gerchis <56065317+Gerchis@users.noreply.github.com>
2020-02-01 14:01:49 +0100

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,18 @@ public class PlayerController : MonoBehaviour
     public float gravity;
     public Transform worldCenter;
     public Rigidbody2D rb;
+    public float speed;
 
     private Vector2 gravityDirection;
+    private Vector3 moveDirection;
+    private Directions inputDirection = Directions.NONE; 
+
+    private enum Directions
+    {
+        NONE,
+        RIGHT,
+        LEFT
+    }
 
     void Start()
     {
@@ -18,13 +28,51 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        gravityDirection.Set(transform.position.x - worldCenter.position.x, transform.position.y - worldCenter.position.y);
+        gravityDirection = worldCenter.position - transform.position;
+
+        moveDirection.Set(-gravityDirection.normalized.y, gravityDirection.normalized.x,0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            inputDirection = Directions.LEFT;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            inputDirection = Directions.RIGHT;
+        }
+        else
+        {
+            inputDirection = Directions.NONE;
+        }
     }
 
     private void FixedUpdate()
     {
+        int movementIndex;
         float delta = Time.fixedDeltaTime * 1000;
+        float distance = 50;
 
-        rb.velocity = gravityDirection.normalized * -gravity * delta;
+        rb.AddForce(-Physics2D.gravity*gravity*gravityDirection.normalized,ForceMode2D.Force);
+
+        //rb.velocity = Vector2.right * Input.GetAxis("Horizontal") * speed * delta;
+
+        switch (inputDirection)
+        {
+            case Directions.NONE:
+                movementIndex = 0;
+                break;
+            case Directions.RIGHT:
+                movementIndex = 1;
+                break;
+            case Directions.LEFT:
+                movementIndex = -1;
+                break;
+            default:
+                movementIndex = 0;
+                break;
+        }
+
+        rb.MovePosition(transform.position + moveDirection.normalized * speed * delta * movementIndex );
+
     }
 }
